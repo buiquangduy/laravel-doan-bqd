21c17eaaeb7b551cc696270106840fb9c2ec7afc
Bui Quang Duy <duy.bui.intern@ntq-solution.com.vn>
2017-05-17 10:56:23 +0700

    edit text

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>product laravel</title>
+<title>Gia Bảo Shop</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="@yield('description')">
 <meta name="author" content="@yield('author')">
